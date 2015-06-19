62f37f56d104ccf98cdf32efea001450c4281591
Remi Aleksander Solås <remi@npolar.no>
2015-06-19 14:20:37 +0200

    Added gitignore, gulpfile and node package

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+//var mocha = require('gulp-mocha');
+
+gulp.task('minify', [], function() {
+	return gulp.src('./linal.js')
+	.pipe(concat('linal.min.js'))
+	.pipe(uglify({ mangle: { keep_fnames: true }, compress: { keep_fnames: true } }))
+	.pipe(gulp.dest('.'));
+});
+
+gulp.task('validate', [], function() {
+	return gulp.src('./linal.js')
+	.pipe(jshint({ multistr: true }))
+	.pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', ['minify'], function() {
+	return gulp.src(['./test/*.js', './test/**/*.js'])
+	.pipe(concat('test.js'))
+	.pipe(gulp.dest('.'))
+	.pipe(mocha({ reporter: 'min' }));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(['./linal.js', 'src/*', './src/**/*'], ['default']);
+});
+
+gulp.task('default', [
+	'validate',
+	'minify'
+]);
