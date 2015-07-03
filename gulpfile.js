fff1593f3432f387f29213476a6475870cd59a81
Remi Aleksander Solås <remi@npolar.no>
2015-07-03 13:34:23 +0200

    Added support for unit testing to gulp and node package

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
-//var mocha = require('gulp-mocha');
+var mocha = require('gulp-mocha');
 
 gulp.task('minify', [], function() {
 	return gulp.src('./linal.js')
@@ -19,10 +19,10 @@ gulp.task('validate', [], function() {
 });
 
 gulp.task('test', ['minify'], function() {
-	return gulp.src(['./test/*.js', './test/**/*.js'])
+	return gulp.src(['./test/linal.js', './test/*.js', './test/**/*.js'])
 	.pipe(concat('test.js'))
 	.pipe(gulp.dest('.'))
-	.pipe(mocha({ reporter: 'min' }));
+	.pipe(mocha({ reporter: 'spec' }));
 });
 
 gulp.task('watch', function() {
